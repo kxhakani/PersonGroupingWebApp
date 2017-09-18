d9c59b78bca2daf3d68c924383454b049ff332ed
blaisni <nick.blais@ontario.ca>
2017-09-18 17:02:43 -0400

    Add project files.

---
+++ PersonGroupWebsite/videoLogin.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="videoLogin.aspx.cs" Inherits="PersonGroupWebsite._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+        <img ID="videoCapture" width="45%" height="90%" src="https://lh3.googleusercontent.com/QrehAdN13wxBJrpttgnD2lQMFJh4fmK0C8UQg8R_-j76EBbfYM-Ie3X72VsG2u8s4mg=w300"/>
+        <img ID="faceDisplay" width="45%" height="90%" src="https://9to5mac.files.wordpress.com/2017/08/face-recognition.jpg?quality=82&strip=all&w=1500&h=750"/>
+    </div>
+
+    <div class="row">
+    </div>
+
+</asp:Content>
---
+++ PersonGroupWebsite/videoLogin.aspx.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using VideoFrameAnalyzer;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System.Threading.Tasks;
+using OpenCvSharp.Extensions;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+
+namespace PersonGroupWebsite
+{
+    /// <summary>
+    /// Interaction logic for videoLogin.xaml
+    /// </summary>
+    public partial class _Default : Page
+    {
+        // Create grabber, with analysis type Face[]. 
+        FrameGrabber<Face[]> grabber = new FrameGrabber<Face[]>();
+
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("00d7358854144900955ef88f7f0b190b", "https://westus.api.cognitive.microsoft.com/face/v1.0");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Find faces in streaming video
+        public void testFaces(Guid person)
+        {
+            grabber = new FrameGrabber<Face[]>();
+
+            //Set up our Face API call.
+            grabber.AnalysisFunction = async frame => await faceServiceClient.DetectAsync(frame.Image.ToMemoryStream(".jpg"));
+
+            //Set up a listener for new frames
+            grabber.NewFrameProvided += (s, e) =>
+            {
+                this.Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    videoCapture.Source = e.Frame.Image.ToBitmapSource();
+                }));
+            };
+
+            // Set up a listener for when we receive a new result from an API call. 
+            grabber.NewResultAvailable += (s, e) =>
+            {
+                if (e.Analysis != null)
+                    Console.WriteLine("New result received for frame acquired at {0}. {1} faces detected", e.Frame.Metadata.Timestamp, e.Analysis.Length);
+
+                this.Dispatcher.BeginInvoke((Action)(async () =>
+                {
+                    BitmapSource image = e.Frame.Image.ToBitmapSource();
+                    Face[] results = e.Analysis;
+                    if (results != null)
+                    {
+                        VerifyResult auth = null;
+
+                        DrawingVisual visual = new DrawingVisual();
+                        DrawingContext drawingContext = visual.RenderOpen();
+                        drawingContext.DrawImage(image, new System.Windows.Rect(0, 0, image.Width, image.Height));
+
+                        if (results.Count() == 1)
+                        {
+                            Face face = results[0];
+
+                            auth = await authenticateUser(face, person);
+
+                            System.Windows.Rect faceRectangle = new System.Windows.Rect(
+                            face.FaceRectangle.Left, face.FaceRectangle.Top,
+                            face.FaceRectangle.Width, face.FaceRectangle.Height);
+
+                            drawingContext.DrawRectangle(Brushes.Transparent, new Pen(Brushes.Red, 3), faceRectangle);
+                        }
+
+                        drawingContext.Close();
+
+                        RenderTargetBitmap render = new RenderTargetBitmap(
+                            image.PixelWidth, image.PixelHeight,
+                            image.DpiX, image.DpiY, PixelFormats.Pbgra32);
+
+                        render.Render(visual);
+                        faceDisplay.Source = render;
+
+                        if (auth != null && auth.IsIdentical)
+                        {
+                            await grabber.StopProcessingAsync();
+                            MessageBox.Show("User has been Authenticated");
+                        }
+                    }
+
+                }));
+            };
+
+            // Tell grabber to call the Face API every 3 seconds.
+            grabber.TriggerAnalysisOnInterval(TimeSpan.FromMilliseconds(3000));
+
+            // Start running.
+            if (grabber.GetNumCameras() > 0)
+                grabber.StartProcessingCameraAsync(lstCameras.SelectedIndex).Wait();
+            else
+                MessageBox.Show("There are no cameras to start");
+        }
+
+        private async Task<VerifyResult> authenticateUser(Face face, Guid person)
+        {
+            VerifyResult auth = null;
+            string loginGroup = cmboLoginGroup.Text;
+
+            auth = await faceServiceClient.VerifyAsync(face.FaceId, loginGroup, person);
+
+            return auth;
+        }
+
+        private Guid getUser()
+        {
+            string loginGroup = cmboLoginGroup.Text;
+            string userName = txtUserName.Text;
+            string PersonId = "";
+            bool connectionError=false;
+
+            string connectionString = null;
+            connectionString = "Server=tcp:sdc-poc-mto-511-test.database.windows.net,1433;Initial Catalog=faceapi-userdb;Persist Security Info=False;User ID=sdcpoc511;Password=Testing511poc;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    String query = "SELECT * FROM dbo.UserInformation WHERE userName=@userName and PersonGroupId=@personGroup;";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@userName", userName);
+                        command.Parameters.AddWithValue("@personGroup", loginGroup);
+
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                PersonId = reader["PersonId"].ToString();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                connectionError = true;
+                MessageBox.Show("Unable to connect to the server. Please try again later.");
+            }
+
+            Guid userId = Guid.Empty;
+            bool isValid = Guid.TryParse(PersonId, out userId);
+
+            if(!isValid && !connectionError)
+                MessageBox.Show("User could not be found");
+
+            return userId;
+        }
+        private async void PopulateComboBox()
+        {
+            string[] groups;
+
+            PersonGroup[] personGroups = await faceServiceClient.ListPersonGroupsAsync();
+
+            groups = personGroups.Select(groupId => groupId.PersonGroupId).ToArray();
+
+            cmboLoginGroup.ItemsSource = groups;
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            StartupWindow startupWindow = new StartupWindow();
+            startupWindow.Show();
+            this.Close();
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            Guid user = getUser();
+
+            if (!user.Equals(Guid.Empty))
+                testFaces(user);
+                
+        }
+
+        private async void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            await grabber.StopProcessingAsync();
+        }
+
+        private void cmboLoginGroup_DropDownOpened(object sender, EventArgs e)
+        {
+            PopulateComboBox();
+        }
+
+        private void lstCameras_Loaded(object sender, RoutedEventArgs e)
+        {
+            int numCameras = grabber.GetNumCameras();
+
+            var comboBox = sender as ComboBox;
+            comboBox.ItemsSource = Enumerable.Range(0, numCameras).Select(i => string.Format("Camera {0}", i + 1));
+            comboBox.SelectedIndex = 0;
+        }
+    }
+}
