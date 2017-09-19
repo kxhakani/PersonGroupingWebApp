a5bd23c35ef0f77ae7612a568b0d316ae5457738
Nick <nick.blais@ontario.ca>
2017-09-19 15:19:41 -0400

    camera changes

--- PersonGroupWebsite/videoLogin.aspx.designer.cs
+++ PersonGroupWebsite/videoLogin.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace PersonGroupWebsite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlImage imgCapture;
+        
+        /// <summary>
+        /// btnLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
     }
 }
